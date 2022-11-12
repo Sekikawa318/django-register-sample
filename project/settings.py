19098b8eb48959552d5cfe3102a3db7da458e3c5
Sekikawa318 <46805444+Sekikawa318@users.noreply.github.com>
2022-11-13 03:19:38 +0900

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=japbnl&5^p%b$)(d!2%n@g9zwj^6mp3&o&d+u#37j2!))ebv0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
